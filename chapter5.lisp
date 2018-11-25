369ab0f814f084bf7d5a99bd1dd74704af09412b
Kazuki Saita <saita@kinoshita-lab.org>
2018-11-25 10:50:58 +0900

    chapter5

---
+++ chapter5.lisp
@@ -0,0 +1,9 @@
+(defun plot (fn min max step)
+  (loop for i from min to max by step do
+       (loop repeat (funcall fn i) do (format t "*"))
+       (format t "~%")))
+
+(plot #'exp 0 4 1/2)
+
+(plot #'(lambda (x) (* 2 x)) 0 10 1)
+(plot (lambda (x) (* 2 x)) 0 10 1)
